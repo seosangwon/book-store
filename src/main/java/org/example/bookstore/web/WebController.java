2021b45aa81e12c3abd0f4e9d75d9c8594575cd0
seosang-won <zmdk1205@naver.com>
2025-01-05 14:01:24 +0900

    chore : initProject

---
+++ src/main/java/org/example/bookstore/web/WebController.java
@@ -0,0 +1,14 @@
+package org.example.bookstore.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+
+}
